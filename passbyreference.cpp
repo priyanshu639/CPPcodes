ff0d97d7cc9a389dc8b1393c3c86154240350153
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-06-26 00:22:09 +0530

    pass by reference by pointers

---
+++ passbyreference.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void changeB(int *ptr){
+    *ptr=20;
+    cout<<*ptr<<"\n";
+}
+int main(){
+    int b=67;
+    changeB(&b);
+    cout<<b<<endl;
+    return 0;
+}
