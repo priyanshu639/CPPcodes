34c41e0823d2b66cb4b09e9c3663395e98aa0a49
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-06-25 23:25:32 +0530

    pointers

--- addressOfPointer.cpp
+++ addressOfPointer.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 int main(){
     int b=123;
+    int *ptr=&b;
+
+    char c='s';
+    char*prt1=&c;
+    float f=3.133893;
+    float *prt=&f;
     cout<<&b<<"\n";
+    cout<<&c<<" = "<<prt1<<endl;
+    cout<<sizeof(ptr)<<" = "<<sizeof(prt1)<<" = "<<sizeof(prt)<<endl;
 }
