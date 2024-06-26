37a03067ca768381e5b56e89f3acf1aab6da1ab3
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-06-26 23:19:37 +0530

    taking input from user  in array

---
+++ input_array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter size of array";
+    cin>>n;
+    int arry[n];
+    for(int i=0;i<n;i++){
+        cin>>arry[i];
+
+
+    }
+    for(int i=0;i<n;i++){
+        cout<<arry[i]<<" ";
+    }cout<<endl;
+    return 0;
+}
