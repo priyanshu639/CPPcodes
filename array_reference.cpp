ee2d62602ddbdc29010657e370c3457b309bd825
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-06-27 16:24:33 +0530

    array can be passed by only reference

---
+++ array_reference.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void function(int arr[]){
+    arr[1]=45;
+}
+
+
+int main(){
+    int arr[]={1,3,6,7,8};
+
+    cout<< *arr<<"\n";//arr[0]
+    cout<<*(arr+1)<<"\n";//arr[1]
+    cout<<*(arr+3)<<"\n";//arr[3]
+    function(arr);
+    cout<<arr[1]<<endl;
+
+    return 0;
+}
